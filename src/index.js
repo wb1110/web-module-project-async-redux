d4acbc0042d0e125a7d03c9c695e74773f1602db
wb1110 <strengthcoachwb@gmail.com>
2021-12-08 16:05:20 -0600

    starting point

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers/rootReducer';
 
 import App from './App';
 import './index.css';
 
+const store = createStore(rootReducer)
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
