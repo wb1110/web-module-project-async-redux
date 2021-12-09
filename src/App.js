af8e84fa1300eec36d9acc7b7cc44a1694973c66
wb1110 <strengthcoachwb@gmail.com>
2021-12-08 21:33:14 -0600

    api restart

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import { createClient } from 'pexels';
 
-function App() {
-  return (
-    <div className="App">
-      Async Redux Project
-    </div>
-  );
+const client = createClient('563492ad6f9170000100000122ff4a6d81ea4728ab1817d88892b721');
+// All requests made with the client will be authenticated
+
+const initialState = []
+
+
+  function App() {
+    useEffect(() => {
+      axios.get(client)
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+      },[])
+    return (
+      <div className="App">
+        Async Redux Project
+      </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import rootReducer from './reducers/rootReducer';
 
 import App from './App';
 import './index.css';
+import thunk from 'redux-thunk';
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
